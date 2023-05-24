ab9bb2782e5077fd359def9279c8c03fdc423bc2
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-05-24 22:30:50 +0800

    Update web.php

--- SME-HR/routes/web.php
+++ SME-HR/routes/web.php
@@ -28,7 +28,6 @@ Route::group([ "middleware" => ['auth:sanctum', config('jetstream.auth_session')
 });
 
 Route::controller(App\Http\Controllers\EmployeeController::class)->group(function () {
-
     Route::get('/Register_Employee', 'CreateEmployee')->name('CreateEmployee');//link to go to reporthomepage
     Route::post('/storeEmployee', 'RegisterEmployee')->name('RegisterEmployee');//link to store the data to the database
     Route::get('/List_Employee', 'ListEmployee')->name('ListEmployee');//link to view list of employee
@@ -40,35 +39,27 @@ Route::controller(App\Http\Controllers\EmployeeController::class)->group(functio
 }); 
 
 Route::controller(App\Http\Controllers\PayrollController::class)->group(function () {
-
     Route::get('/List_Payroll', 'ListPayroll')->name('ListPayroll');//link to go to payroll list page
     Route::get('/Generate_Payslip', 'GeneratePayslip')->name('GeneratePayslip');//link to go to generate payslip
     Route::get('/View_Payslip', 'ViewPayslip')->name('ViewPayslip');//link to go to generate payslip
-
 });
 
 Route::controller(App\Http\Controllers\ClaimController::class)->group(function () {
-
     Route::get('/Apply_claim', 'ApplyClaim')->name('ApplyClaim');//link to go to reporthomepage
     Route::get('/List_Claim', 'ListClaim')->name('ListClaim');//link to go to claim list page
-
 });
 
 Route::controller(App\Http\Controllers\LeaveController::class)->group(function () {
-
     Route::get('/Apply_Leave', 'ApplyLeave')->name('ApplyLeave');//link to go to apply leave page
-
 });
 
 Route::controller(App\Http\Controllers\EAFormController::class)->group(function () {
-
     Route::get('/EAForm', 'EAForm')->name('EAForm');//link to view list of employee 
     Route::get('viewEAForm/{id}', 'ListEAForm')->name('ListEAForm');//link to go to view ea form
     Route::get('Add_EAForm/{id}', 'addEAForm')->name('addEAForm');//link to go to add ea form
 });
 
 Route::controller(App\Http\Controllers\PositionController::class)->group(function () {
-
     Route::get('/List_Position', 'ListPosition')->name('ListPosition');//link to list of position page
     Route::get('Add_Position', 'addPosition')->name('addPosition');//link to go to add position page
     Route::post('/Store_Position', 'StorePosition')->name('StorePosition');//link to go to store the postion data into db
@@ -79,9 +70,7 @@ Route::controller(App\Http\Controllers\SalaryTypeController::class)->group(funct
 });
 
 Route::controller(App\Http\Controllers\ClaimTypeController::class)->group(function () {
-
     Route::get('/ListClaimType', 'ListClaimType')->name('ListClaimType');//link to list of claim typepage
     Route::get('AddClaimType', 'addClaimType')->name('addClaimType');//link to go to add claim type page
     Route::post('/StoreClaimType', 'StoreClaimType')->name('StoreClaimType');//link to go to store the claim type data into db
-
 });
