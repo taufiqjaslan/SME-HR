e006dcf6d834285392af5341fb8cc5105169ce80
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-05-25 10:50:52 +0800

    Update AddEAForm.blade.php

--- SME-HR/resources/views/ManageEAForm/AddEAForm.blade.php
+++ SME-HR/resources/views/ManageEAForm/AddEAForm.blade.php
@@ -336,8 +336,7 @@
                                     <hr>
                                     <br>
                                     <div class="form-actions text-center">
-                                        <button class="btn btn-primary float-md-right" id="generate_button">
-                                            <i class="fa fa-dot-circle-o"></i>&nbsp;&nbsp;Save</button>
+                                        <button class="btn btn-primary float-md-right" id="">Save</button>
                                     </div>
                                 </form>
                             </div>
