e14688514494e1a7f69c7e5ac552c528f88ccd27
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-01 09:10:42 +0800

    Update LeaveRecord.php

--- SME-HR/app/Models/LeaveRecord.php
+++ SME-HR/app/Models/LeaveRecord.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class LeaveRecord extends Model
 {
     use HasFactory;
+
+    protected $table = 'leaves';
+
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'leave_type_id',
+        'detail',
+        'attachment',
+        'status',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(EmployeeRecord::class, 'user_id');
+    }
+
+    public function leaveType()
+    {
+        return $this->belongsTo(LeaveTypeRecord::class, 'leave_type_id');
+    }
 }
