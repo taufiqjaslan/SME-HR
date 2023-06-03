165402b9b6f4612202a7bcfb1b15828a9b786a24
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-03 23:39:53 +0800

    Update LeaveController.php

--- SME-HR/app/Http/Controllers/LeaveController.php
+++ SME-HR/app/Http/Controllers/LeaveController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Response;
 use App\Models\EmployeeRecord;
 use App\Models\LeaveTypeRecord;
 use App\Models\LeaveRecord;
+use Illuminate\Support\Facades\File;
 
 class LeaveController extends Controller
 {
@@ -54,46 +55,100 @@ class LeaveController extends Controller
             'status' => 1,
         ]);
 
-        return redirect()->route('ApplyLeave');
+        return redirect()->route('listLeave');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    //List leave function
+    public function listLeave()
     {
-        //
+        // Retrieve all payroll records and include the associated employee data and salary_type data
+        $leaveRecords = LeaveRecord::with('employee', 'leaveType')
+            ->join('users', 'leaves.user_id', '=', 'Users.id')
+            ->join('leave_types', 'leaves.leave_type_id', '=', 'leave_types.id')
+            ->select('leaves.*', 'users.name', 'leave_types.leave_name')
+            ->get();
+
+        return view('ManageLeave.LeaveList', ["leaveRecords" => $leaveRecords]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function viewLeave(string $id)
     {
-        //
+        $leaveInfo = Leaverecord::find($id);
+        $employeeInfo = EmployeeRecord::all(); // Fetch employee from the database
+        $leaveTypeInfo = LeaveTypeRecord::all(); // Fetch leavetype from the database
+
+        return view('ManageLeave.ViewLeave', compact('leaveInfo', 'employeeInfo', 'leaveTypeInfo'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function editLeave(string $id)
     {
-        //
+        $leaveInfo = Leaverecord::find($id);
+        $employeeInfo = EmployeeRecord::all(); // Fetch employee from the database
+        $leaveTypeInfo = LeaveTypeRecord::all(); // Fetch leavetype from the database
+
+        return view('ManageLeave.EditLeave', compact('leaveInfo', 'employeeInfo', 'leaveTypeInfo'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function updateLeave(Request $request, $id)
     {
-        //
+        // Update employee info from the database
+        $updateInfo = LeaveRecord::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'user_id' => 'required',
+            'leave_type_id' => 'required',
+            'detail' => 'required',
+            'start_date' => 'nullable',
+            'end_date' => 'nullable',
+        ]);
+
+        if ($request->hasFile('attachment')) {
+            $file = $request->file('attachment');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move('uploads/attachment/', $filename);
+
+            // Update the attachment field in the database
+            $validatedData['attachment'] = $filename;
+
+            // Delete the previous attachment file if it exists
+            if (!empty($updateInfo->attachment)) {
+                $previousFile = 'uploads/attachment/' . $updateInfo->attachment;
+                if (File::exists($previousFile)) {
+                    File::delete($previousFile);
+                }
+            }
+        }
+
+        $updateInfo->update($validatedData);
+        return redirect()->route('ListLeave')->with('success', 'Leave Info updated successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function deleteLeave($id)
     {
-        //
+        $leaveRecord = LeaveRecord::find($id);
+
+        if (!$leaveRecord) {
+            return redirect()->back()->with('error', 'Leave record not found.');
+        }
+
+        // delete record
+        $leaveRecord->delete();
+        session()->flash('success', 'Leave record deleted successfully.');
+
+        // redirect to previous page
+        return redirect()->back();
     }
 }
