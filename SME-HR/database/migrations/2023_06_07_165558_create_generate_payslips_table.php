3696b04880e5c5597877533d741c0c6367887776
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-08 01:23:26 +0800

    Create 2023_06_07_165558_create_generate_payslips_table.php

---
+++ SME-HR/database/migrations/2023_06_07_165558_create_generate_payslips_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('generate_payslips', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); //foreign key
+            $table->decimal('basic_salary'); 
+            $table->decimal('kwsp_staff'); 
+            $table->decimal('kwsp_company'); 
+            $table->decimal('socso_staff');
+            $table->decimal('socso_company'); 
+            $table->decimal('eis_staff'); 
+            $table->decimal('eis_company'); 
+            $table->decimal('zakat')->nullable();
+            $table->decimal('allowance')->nullable();
+            $table->decimal('bonus')->nullable();
+            $table->decimal('deduction')->nullable();
+            $table->decimal('netpay')->nullable();
+            $table->integer('year')->nullable();
+            $table->integer('month')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');//foreign key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('generate_payslips');
+    }
+};
