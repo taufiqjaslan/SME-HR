41cfa53b96f6364503f298cac4a9549c6fea66c9
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-15 00:14:54 +0800

    Update navbar.blade.php

--- SME-HR/resources/views/components/navbar.blade.php
+++ SME-HR/resources/views/components/navbar.blade.php
@@ -11,70 +11,81 @@ $user = auth()->user();
         <h1 class="font-weight-bold text-2xl text-white">{{ config('app.name', 'Laravel') }}</h1>
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown dropdown-list-toggle">
-            <a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep">
-                <i class="far fa-bell"></i>
-            </a>
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
             <div class="dropdown-menu dropdown-list dropdown-menu-right">
                 <div class="dropdown-header">Notifications
                     <div class="float-right">
                         <a href="#">Mark All As Read</a>
                     </div>
                 </div>
-                <a href="#" class="dropdown-item">
-                    <div class="dropdown-item-desc">
-                        <b>You</b> and <b>Dedik Sugiharto</b> are now friends
-                        <div class="time">10 Hours Ago</div>
-                    </div>
-                </a>
-                <a href="#" class="dropdown-item">
-                    <div class="dropdown-item-desc">
-                        <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
-                        <div class="time">12 Hours Ago</div>
-                    </div>
-                </a>
+                <div class="dropdown-list-content dropdown-list-icons">
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-icon bg-primary text-white">
+                            <i class="fas fa-code"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Template update is available now!
+                            <div class="time text-primary">2 Min Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>You</b> and <b>Dedik Sugiharto</b> are now friends
+                            <div class="time">10 Hours Ago</div>
+                        </div>
+                    </a>
+                </div>
                 <div class="dropdown-footer text-center">
                     <a href="#">View All <i class="fas fa-chevron-right"></i></a>
                 </div>
             </div>
         </li>
+
+
+        <!-- Settings Dropdown -->
         <li class="dropdown">
-            <a href="#" data-turbolinks="false" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <div class="nav-link nav-link-lg nav-link-user" data-toggle="dropdown">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->username }}" />
+                <button class="flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                    <div class="d-flex flex-column text-right pr-3">
+                        <span class="ml-2 d-sm-none d-lg-inline-block" style="opacity: 0.5; ">{{ Auth::user()->username }}</span>
+                        <span class="ml-2 d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</span>
+                    </div>
+                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                 </button>
+
                 @else
-                <span class="inline-flex rounded-md">
-                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                        {{ Auth::user()->username }}
-
-                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                </span>
+                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                    {{ Auth::user()->name }}
+                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                </button>
                 @endif
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a href="/user/profile" class="dropdown-item has-icon">
-                        <i class="far fa-user"></i> Profile
-                    </a>
-                    @if (request()->get('is_admin'))
-                    <a href="/setting" class="dropdown-item has-icon">
-                        <i class="fas fa-cog"></i> Setting
-                    </a>
-                    @endif
-                    <div class="dropdown-divider"></div>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger" onclick="event.preventDefault();this.closest('form').submit();">
-                            <i class="fas fa-sign-out-alt"></i> Logout
-                        </a>
-                    </form>
+            </div>
+            <div class="dropdown-menu dropdown-menu-right">
+                <!-- Account Management -->
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Account') }}
                 </div>
-            </a>
+
+                <a href="{{ route('profile.show') }}" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> Profile
+                </a>
+
+                <div class="border-t border-gray-100"></div>
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <button type="submit" class="dropdown-item has-icon text-danger flex items-center">
+                        <i class="fas fa-sign-out-alt"></i><span class="text-sm">Logout</span>
+                    </button>
+                </form>
+            </div>
         </li>
     </ul>
-
 </nav>
