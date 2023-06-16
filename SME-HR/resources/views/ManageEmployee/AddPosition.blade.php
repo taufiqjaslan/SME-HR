1398d38d5b687ee3c5c46fd2dfb270b6633232bf
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-16 20:22:07 +0800

    Update AddPosition.blade.php

--- SME-HR/resources/views/ManageEmployee/AddPosition.blade.php
+++ SME-HR/resources/views/ManageEmployee/AddPosition.blade.php
@@ -9,46 +9,89 @@
         </div>
     </x-slot>
 
-    <div>
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <!--begin::Tab-->
-                        <div class="tab-pane show active px-7" id="kt_user_edit_tab_1" role="tabpanel">
-                            <!--begin::Row-->
-                            <div class="card-header">
-                                <h1 class="card-title"><i class="fas fa-user">&nbsp;&nbsp;&nbsp;</i>Position Information</h1>
-                            </div>
-                            <div class="card-content collpase show">
-                                <div class="card-body">
-                                    <form method="POST" class="form form-horizontal" action="{{route('StorePosition')}}">
-                                        @csrf
-                                        <div class="form-body">
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group row">
-                                                        <label class="col-md-3 label-control">Type of Position</label>
-                                                        <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="text" placeholder="Position Name" name="position_name" id="position_name">
-                                                        </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <!--begin::Tab-->
+                    <div class="tab-pane show active px-7" id="kt_user_edit_tab_1" role="tabpanel">
+                        <!--begin::Row-->
+                        <div class="card-header">
+                            <h1 class="card-title"><i class="fas fa-user">&nbsp;&nbsp;&nbsp;</i>Position Information</h1>
+                        </div>
+                        <div class="card-content collpase show">
+                            <div class="card-body">
+                                <form method="POST" class="form form-horizontal" action="{{route('StorePosition')}}" id="positionForm">
+                                    @csrf
+                                    <div class="form-body">
+                                        <div class="row">
+                                            <div class="col-md-9">
+                                                <div class="form-group row">
+                                                    <label class="col-md-3 label-control">Type of Position</label>
+                                                    <div class="col-md-9 mx-auto">
+                                                        <input class="form-control border-primary" type="text" placeholder="Position Name" name="position_name" id="position_name" required>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <hr>
-                                        <br>
-                                        <div class="form-actions text-center">
-                                            <button class="btn btn-primary float-md-right" id="">Save</button>
-                                        </div>
-                                    </form>
-                                </div>
+                                    </div>
+                                    <hr>
+                                    <br>
+                                    <div class="form-actions text-center">
+                                        <button class="btn btn-primary float-md-right" id="addPosition">Save</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                </div><!--end card-body-->
-            </div><!--end card-->
-        </div> <!--end col-->
-    </div>
+                </div>
+            </div><!--end card-body-->
+        </div><!--end card-->
+    </div> <!--end col-->
 
 </x-app-layout>
+
+<script>
+    $(document).ready(function() {
+        $("#addPosition").click(function(event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Manually trigger form validation
+            if ($("#positionForm")[0].checkValidity()) {
+                // Show SweetAlert dialog
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You want to add this data!',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#6777ef',
+                    cancelButtonColor: '$secondary',
+                    confirmButtonText: 'Yes, add it!',
+                    dangerMode: true,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Show success message
+                        Swal.fire({
+                            title: 'Success!',
+                            text: 'Your data has been saved.',
+                            icon: 'success',
+                            showConfirmButton: true // Show the "OK" button
+                        }).then(() => {
+                            // Submit the form here
+                            $("#positionForm").submit();
+                        });
+                    }
+                });
+            } else {
+                // Handle invalid form
+                Swal.fire({
+                    title: 'Invalid Form',
+                    text: 'Please fill in all the required fields.',
+                    icon: 'error',
+                    showConfirmButton: true, // Show the "OK" button
+                    confirmButtonColor: '#6777ef',
+                });
+            }
+        });
+    });
+</script>
