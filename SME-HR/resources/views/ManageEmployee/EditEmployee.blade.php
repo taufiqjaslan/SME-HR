ce0ff54490a79bc9d29bd7e4cac6307096d41e30
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-16 20:22:13 +0800

    Update EditEmployee.blade.php

--- SME-HR/resources/views/ManageEmployee/EditEmployee.blade.php
+++ SME-HR/resources/views/ManageEmployee/EditEmployee.blade.php
@@ -22,7 +22,7 @@
                             </div>
                             <div class="card-content collpase show">
                                 <div class="card-body">
-                                    <form method="POST" class="form form-horizontal" action="{{route('updateEmployee' , ['id' => $employeeInfo->id])}}">
+                                    <form method="POST" class="form form-horizontal" action="{{route('updateEmployee' , ['id' => $employeeInfo->id])}}" id="updateForm">
                                         @csrf
                                         @method('PUT')
                                         <div class="form-body">
@@ -35,7 +35,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Full Name</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Full Name" id="name" name="name" value="{{ old('name', $employeeInfo->name) }}">
+                                                            <input type="text" class="form-control border-primary" placeholder="Full Name" id="name" name="name" value="{{ old('name', $employeeInfo->name) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -43,7 +43,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Username</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Username" name="username" id="username" value="{{ old('username', $employeeInfo->username) }}">
+                                                            <input type="text" class="form-control border-primary" placeholder="Username" name="username" id="username" value="{{ old('username', $employeeInfo->username) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -53,7 +53,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Identification Number</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Identification Number" name="ic" id="ic" value="{{ old('ic', $employeeInfo->ic) }}">
+                                                            <input type="text" class="form-control border-primary" placeholder="Identification Number" name="ic" id="ic" value="{{ old('ic', $employeeInfo->ic) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -61,7 +61,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Phone Number</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Phone Number" name="phone_number" id="phone_number" value="{{ old('phone_number', $employeeInfo->phone_number) }}">
+                                                            <input type="text" class="form-control border-primary" placeholder="Phone Number" name="phone_number" id="phone_number" value="{{ old('phone_number', $employeeInfo->phone_number) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -71,7 +71,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Email</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="email" placeholder="Email" name="email" id="email" value="{{ old('email', $employeeInfo->email) }}">
+                                                            <input class="form-control border-primary" type="email" placeholder="Email" name="email" id="email" value="{{ old('email', $employeeInfo->email) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -79,7 +79,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Password</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="password" placeholder="Password" name="password" id="password" value="{{ old('password', $employeeInfo->password) }}">
+                                                            <input class="form-control border-primary" type="password" placeholder="Password" name="password" id="password" value="{{ old('password', $employeeInfo->password) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -87,7 +87,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Confirmation Password</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="password" placeholder="Confirm Password" name="confirm_password" id="password" value="{{ old('password', $employeeInfo->password) }}">
+                                                            <input class="form-control border-primary" type="password" placeholder="Confirm Password" name="confirm_password" id="password" value="{{ old('password', $employeeInfo->password) }}" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -102,9 +102,12 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Position</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="position_id" class="form-control border-primary" id="position_id">
-                                                            <option value="1" {{ old('position_id', $employeeInfo->position_id) == '1' ? 'selected' : '' }}>Cashier</option>
-                                                            <option value="2" {{ old('position_id', $employeeInfo->position_id) == '2' ? 'selected' : '' }}>Cooker</option>
+                                                        <select name="position_id" class="form-control border-primary" id="position_id" required>
+                                                            @foreach ($positions as $position)
+                                                            <option value="{{ $position->id }}" {{ old('position_id', $employeeInfo->position_id) == $position->id ? 'selected' : '' }}>
+                                                                {{ $position->position_name }}
+                                                            </option>
+                                                            @endforeach
                                                         </select>
                                                     </div>
                                                 </div>
@@ -113,10 +116,12 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">User Type</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="user_type_id" class="form-control border-primary" id="user_type_id">
-                                                            <option value="1" {{ old('user_type_id', $employeeInfo->user_type_id) == '1' ? 'selected' : '' }}>Administrator</option>
-                                                            <option value="2" {{ old('user_type_id', $employeeInfo->user_type_id) == '2' ? 'selected' : '' }}>Manager</option>
-                                                            <option value="3" {{ old('user_type_id', $employeeInfo->user_type_id) == '3' ? 'selected' : '' }}>Staff</option>
+                                                        <select name="user_type_id" class="form-control border-primary" id="user_type_id" required>
+                                                            @foreach ($userType as $userTypes)
+                                                            <option value="{{ $userTypes->id }}" {{ old('user_type_id', $employeeInfo->user_type_id) == $userTypes->id ? 'selected' : '' }}>
+                                                                {{ $userTypes->user_type_name }}
+                                                            </option>
+                                                            @endforeach
                                                         </select>
                                                     </div>
                                                 </div>
@@ -127,7 +132,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Start Date </label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="date" class="form-control border-primary" placeholder="Start Date" id="start_date" name="start_date" value="{{ old('start_date', $employeeInfo->start_date) }}">
+                                                        <input type="date" class="form-control border-primary" placeholder="Start Date" id="start_date" name="start_date" value="{{ old('start_date', $employeeInfo->start_date) }}" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -135,7 +140,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">End Date</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="date" class="form-control border-primary" placeholder="" name="end_date" id="end_date" value="{{ old('end_date', $employeeInfo->end_date) }}">
+                                                        <input type="date" class="form-control border-primary" placeholder="" name="end_date" id="end_date" value="{{ old('end_date', $employeeInfo->end_date) }}" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -145,7 +150,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Gender</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="gender" class="form-control border-primary" id="gender">
+                                                        <select name="gender" class="form-control border-primary" id="gender" required>
                                                             <option value="1" {{ old('gender', $employeeInfo->gender) == '1' ? 'selected' : '' }}>Female</option>
                                                             <option value="2" {{ old('gender', $employeeInfo->gender) == '2' ? 'selected' : '' }}>Male</option>
                                                         </select>
@@ -156,7 +161,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Address</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <textarea rows="6" class="form-control border-primary" name="address" placeholder="Address" id="address">{{ old('address', $employeeInfo->address) }}</textarea>
+                                                        <textarea rows="6" class="form-control border-primary" name="address" placeholder="Address" id="address" required>{{ old('address', $employeeInfo->address) }}</textarea>
                                                     </div>
                                                 </div>
                                             </div>
@@ -166,7 +171,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Basic Salary</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="text" class="form-control border-primary" placeholder="" name="basic_salary" id="basic_salary" value="{{ old('basic_salary', $employeeInfo->basic_salary) }}">
+                                                        <input type="text" class="form-control border-primary" placeholder="" name="basic_salary" id="basic_salary" value="{{ old('basic_salary', $employeeInfo->basic_salary) }}" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -174,7 +179,7 @@
                                         <hr>
                                         <br>
                                         <div class="form-actions text-center">
-                                            <button class="btn btn-primary float-md-right">Update</button>
+                                            <button class="btn btn-primary float-md-right" id="updateButton">Update</button>
                                         </div>
                                     </form>
                                 </div>
@@ -187,3 +192,48 @@
     </div>
 
 </x-app-layout>
+
+<script>
+    $(document).ready(function() {
+        $("#updateButton").click(function(event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Manually trigger form validation
+            if ($("#updateForm")[0].checkValidity()) {
+                // Show SweetAlert dialog
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You want to update this data!',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#6777ef',
+                    cancelButtonColor: '$secondary',
+                    confirmButtonText: 'Yes, update it!',
+                    dangerMode: true,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Show success message
+                        Swal.fire({
+                            title: 'Success!',
+                            text: 'Your data has been updated.',
+                            icon: 'success',
+                            showConfirmButton: true // Show the "OK" button
+                        }).then(() => {
+                            // Submit the form here
+                            $("#updateForm").submit();
+                        });
+                    }
+                });
+            } else {
+                // Handle invalid form
+                Swal.fire({
+                    title: 'Invalid Form',
+                    text: 'Please fill in all the required fields.',
+                    icon: 'error',
+                    showConfirmButton: true, // Show the "OK" button
+                    confirmButtonColor: '#6777ef',
+                });
+            }
+        });
+    });
+</script>
