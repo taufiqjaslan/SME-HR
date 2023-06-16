afb9dbe1ca31430ce22d295a0461e4bf96566ffa
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-16 20:21:18 +0800

    Update auth.php

--- SME-HR/lang/id/auth.php
+++ SME-HR/lang/id/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'Kredensial ini tidak cocok dengan yang ada pada database.',
-    'password' => 'Kata sandi yang diberikan salah.',
-    'throttle' => 'Terlalu banyak upaya masuk. Silakan coba lagi dalam :seconds detik.',
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
