d182d9c3cb5a571026b46d1b1d6ac6325123950c
Muhammad Taufiq Bin Mohd Jaslan <topeqjazlan00@gmail.com>
2023-06-16 20:21:55 +0800

    Update AddEmployee.blade.php

--- SME-HR/resources/views/ManageEmployee/AddEmployee.blade.php
+++ SME-HR/resources/views/ManageEmployee/AddEmployee.blade.php
@@ -22,7 +22,7 @@
                             </div>
                             <div class="card-content collpase show">
                                 <div class="card-body">
-                                    <form method="POST" class="form form-horizontal" action="{{route('RegisterEmployee')}}">
+                                    <form method="POST" class="form form-horizontal" action="{{route('RegisterEmployee')}}" id="registerEmployee">
                                         @csrf
                                         <div class="form-body">
                                             <h4 class="form-section"><i class="fas fa-file-alt">&nbsp;&nbsp;&nbsp;</i>Employee Details</h4>
@@ -34,7 +34,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Full Name</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Full Name" id="name" name="name">
+                                                            <input type="text" class="form-control border-primary" placeholder="Full Name" id="name" name="name" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -42,7 +42,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Username</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Username" name="username" id="username">
+                                                            <input type="text" class="form-control border-primary" placeholder="Username" name="username" id="username" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -52,7 +52,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Identification Number</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Identification Number" name="ic" id="ic">
+                                                            <input type="text" class="form-control border-primary" placeholder="Identification Number" name="ic" id="ic" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -60,7 +60,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Phone Number</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input type="text" class="form-control border-primary" placeholder="Phone Number" name="phone_number" id="phone_number">
+                                                            <input type="text" class="form-control border-primary" placeholder="Phone Number" name="phone_number" id="phone_number" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -70,7 +70,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Email</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="email" placeholder="Email" name="email" id="email">
+                                                            <input class="form-control border-primary" type="email" placeholder="Email" name="email" id="email" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -78,7 +78,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Password</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="password" placeholder="Password" name="password" id="password">
+                                                            <input class="form-control border-primary" type="password" placeholder="Password" name="password" id="password" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -86,7 +86,7 @@
                                                     <div class="form-group row">
                                                         <label class="col-md-3 label-control">Confirmation Password</label>
                                                         <div class="col-md-9 mx-auto">
-                                                            <input class="form-control border-primary" type="password" placeholder="Confirm Password" name="password" id="password">
+                                                            <input class="form-control border-primary" type="password" placeholder="Confirm Password" name="password" id="password" required>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -101,7 +101,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Position</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="position_id" class="form-control border-primary" id="position_id">
+                                                        <select name="position_id" class="form-control border-primary" id="position_id" required>
                                                             <option disabled value="" selected hidden>Select</option>
                                                             @foreach($userTypesAndPositions['positions'] as $position)
                                                             <option value="{{ $position->id }}">{{ $position->position_name }}</option>
@@ -114,7 +114,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">User Type</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="user_type_id" class="form-control border-primary" id="user_type_id">
+                                                        <select name="user_type_id" class="form-control border-primary" id="user_type_id" required>
                                                             <option disabled value="" selected hidden>Select</option>
                                                             @foreach($userTypesAndPositions['userTypes'] as $userType)
                                                             <option value="{{ $userType->id }}">{{ $userType->user_type_name }}</option>
@@ -129,7 +129,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Start Date </label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="date" class="form-control border-primary" placeholder="Start Date" id="start_date" name="start_date">
+                                                        <input type="date" class="form-control border-primary" placeholder="Start Date" id="start_date" name="start_date" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -137,7 +137,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">End Date</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="date" class="form-control border-primary" placeholder="" name="end_date" id="end_date">
+                                                        <input type="date" class="form-control border-primary" placeholder="" name="end_date" id="end_date" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -147,7 +147,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Gender</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <select name="gender" class="form-control border-primary" id="gender">
+                                                        <select name="gender" class="form-control border-primary" id="gender" required>
                                                             <option disabled value="" selected hidden>Select</option>
                                                             <option value="1">Female</option>
                                                             <option value="2">Male</option>
@@ -159,7 +159,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Address</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <textarea rows="6" class="form-control border-primary" name="address" placeholder="Address" id="address"></textarea>
+                                                        <textarea rows="6" class="form-control border-primary" name="address" placeholder="Address" id="address" required></textarea>
                                                     </div>
                                                 </div>
                                             </div>
@@ -169,7 +169,7 @@
                                                 <div class="form-group row">
                                                     <label class="col-md-3 label-control">Basic Salary</label>
                                                     <div class="col-md-9 mx-auto">
-                                                        <input type="text" class="form-control border-primary" placeholder="" name="basic_salary" id="basic_salary">
+                                                        <input type="text" class="form-control border-primary" placeholder="" name="basic_salary" id="basic_salary" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -190,3 +190,48 @@
     </div>
 
 </x-app-layout>
+
+<script>
+    $(document).ready(function() {
+        $("#generate_button").click(function(event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Manually trigger form validation
+            if ($("#registerEmployee")[0].checkValidity()) {
+                // Show SweetAlert dialog
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You want to add this data!',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#6777ef',
+                    cancelButtonColor: '$secondary',
+                    confirmButtonText: 'Yes, add it!',
+                    dangerMode: true,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Show success message
+                        Swal.fire({
+                            title: 'Success!',
+                            text: 'Your data has been saved.',
+                            icon: 'success',
+                            showConfirmButton: true // Show the "OK" button
+                        }).then(() => {
+                            // Submit the form here
+                            $("#registerEmployee").submit();
+                        });
+                    }
+                });
+            } else {
+                // Handle invalid form
+                Swal.fire({
+                    title: 'Invalid Form',
+                    text: 'Please fill in all the required fields.',
+                    icon: 'error',
+                    showConfirmButton: true, // Show the "OK" button
+                    confirmButtonColor: '#6777ef',
+                });
+            }
+        });
+    });
+</script>
